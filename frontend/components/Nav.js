8d0ecc9a861d9c92a2a785de9226b56673622c03
mycak <mycak@wp.pl>
2020-12-30 14:05:13 +0100

    deleteFromCart component added

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useMutation } from '@apollo/client';
+import CartCount from './CartCount';
 import Signout from './Signout';
 import NavStyles from './styles/NavStyles';
 import User from './User';
@@ -22,26 +23,35 @@ const Nav = () => {
             </NavStyles>
           );
         }
-        return (
-          <NavStyles>
-            <Link href="/items">
-              <a>Shop</a>
-            </Link>
-            <Link href="/sell">
-              <a>Sell</a>
-            </Link>
-            <Link href="/orders">
-              <a>Orders</a>
-            </Link>
-            <Link href="/me">
-              <a>Account</a>
-            </Link>
-            <Signout />
-            <button type="button" onClick={toggleCart}>
-              My Cart
-            </button>
-          </NavStyles>
-        );
+        if (data && data.me) {
+          return (
+            <NavStyles>
+              <Link href="/items">
+                <a>Shop</a>
+              </Link>
+              <Link href="/sell">
+                <a>Sell</a>
+              </Link>
+              <Link href="/orders">
+                <a>Orders</a>
+              </Link>
+              <Link href="/me">
+                <a>Account</a>
+              </Link>
+              <Signout />
+              <button type="button" onClick={toggleCart}>
+                My Cart
+                <CartCount
+                  count={data.me.cart.reduce(
+                    (tally, cartItem) => tally + cartItem.quantity,
+                    0
+                  )}
+                />
+              </button>
+            </NavStyles>
+          );
+        }
+        return <p>Loading...</p>;
       }}
     </User>
   );
--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { useMutation, gql } from '@apollo/client';
+import { CURRENT_USER_QUERY } from './User';
 
 const REMOVE_FROM_CART_MUTATION = gql`
   mutation removeFromCart($id: ID!) {
@@ -24,14 +25,36 @@ const RemoveFromCart = ({ id }) => {
   const [removeFromCartMutation, { loading }] = useMutation(
     REMOVE_FROM_CART_MUTATION
   );
-
+  const update = (cache, payload) => {
+    console.log('Running remove from cart update fn');
+    // 1. first read the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    console.log(data);
+    // 2. remove that item from the cart
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(
+      (cartItem) => cartItem.id !== cartItemId
+    );
+    // 3. write it back to the cache
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
   return (
     <BigButton
       disabled={loading}
       onClick={() => {
-        removeFromCartMutation({ variables: { id } }).catch((err) =>
-          alert(err.message)
-        );
+        removeFromCartMutation({
+          variables: { id },
+          update: (cache, payload) => {
+            update(cache, payload);
+          },
+          optimisticResponse: {
+            __typename: 'Mutation',
+            removeFromCart: {
+              __typename: 'CartItem',
+              id,
+            },
+          },
+        }).catch((err) => alert(err.message));
       }}
       title="Delete Item"
     >
