d6202ce33da34e9abf106e63010d0ab4d233d51b
mycak <mycak@wp.pl>
2021-01-05 14:51:01 +0100

    secure and sameSite options added

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -84,12 +84,12 @@ const Mutations = {
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
     // We set the jwt as a cookie on the response
     ctx.response.cookie('token', token, {
-      domain: process.env.NODE_ENV === 'production' ? undefined : process.env.APP_DOMAIN,
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "None",
-      path: '/'
+      domain: process.env.NODE_ENV === 'development' ? undefined : process.env.APP_DOMAIN,
+                secure: process.env.NODE_ENV === 'development' ? false : true,
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+                sameSite: 'lax',
+                path: '/'
     });
     // Finalllllly we return the user to the browser
     return user;
@@ -109,12 +109,10 @@ const Mutations = {
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
     // 4. Set the cookie with the token
     ctx.response.cookie('token', token, {
-      domain: process.env.NODE_ENV === 'production' ? undefined : process.env.APP_DOMAIN,
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+      maxAge: 1000 * 60 * 60 * 24 * 365,
       secure: process.env.NODE_ENV === "production",
       sameSite: "None",
-      path: '/'
     });
     // 5. Return the user
     return user;
