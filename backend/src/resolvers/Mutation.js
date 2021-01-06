7c06731602585ee113e4f8139bff0f1279af13ee
mycak <mycak@wp.pl>
2021-01-06 18:52:03 +0100

    fixing

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -84,12 +84,11 @@ const Mutations = {
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
     // We set the jwt as a cookie on the response
     ctx.response.cookie('token', token, {
-      // domain: process.env.NODE_ENV === 'development' ? undefined : process.env.APP_DOMAIN,
+      domain: process.env.NODE_ENV === 'development' ? undefined : process.env.APP_DOMAIN,
                 secure: process.env.NODE_ENV === 'development' ? false : true,
                 httpOnly: true,
                 maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
                 sameSite: 'lax',
-                // path: '/'
     });
     // Finalllllly we return the user to the browser
     return user;
@@ -109,12 +108,11 @@ const Mutations = {
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
     // 4. Set the cookie with the token
     ctx.response.cookie('token', token, {
-      // domain: process.env.NODE_ENV === 'development' ? undefined : process.env.APP_DOMAIN,
+      domain: process.env.NODE_ENV === 'development' ? undefined : process.env.APP_DOMAIN,
                 secure: process.env.NODE_ENV === 'development' ? false : true,
                 httpOnly: true,
                 maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
                 sameSite: 'Lax',
-                // path: '/'
     });
     // 5. Return the user
     return user;
