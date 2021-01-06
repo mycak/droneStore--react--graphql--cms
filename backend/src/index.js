ac17dc02c6fe970c048d1f465524059cfd6d99c5
mycak <mycak@wp.pl>
2021-01-06 12:26:45 +0100

    credentials

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,14 +32,13 @@ server.express.use(async (req, res, next) => {
   req.user = user;
   next();
 });
-//esa
 server.start(
-  // {
-  //   cors: {
-  //     credentials: true,
-  //     origin: process.env.FRONTEND_URL,
-  //   },
-  // },
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
   deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
